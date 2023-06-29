adee125ad20787b4572e40ed23f7ac33c256dd90
sonnned <alejo1garciasosa@gmail.com>
2023-06-29 15:29:21 -0500

    Swiper ready

--- src/components/Slide.tsx
+++ src/components/Slide.tsx
@@ -1,16 +1,19 @@
-import { FC, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { API_ACCESS_TOKEN, BASE_URL, endPoints } from '../config/fetcher';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/scrollbar';
 
 interface SlideData {
   backdrop_path: string;
   id: number;
   title: string;
   overview: string;
+  poster_path: string;
 }
 
 const Slide = () => {
   const [movies, setMovies] = useState<any>([]);
-  const [slideIndex, setSlideIndex] = useState<number>(0);
   const [slideData, setSlideData] = useState<Array<SlideData>>([]);
 
   async function fetchData() {
@@ -35,6 +38,7 @@ const Slide = () => {
           id: item.id,
           title: item.title,
           overview: item.overview,
+          poster_path: item.poster_path,
         };
       });
       setSlideData(filtered);
@@ -50,30 +54,46 @@ const Slide = () => {
   }, [movies]);
 
   return (
-    <section className="relative h-[100vh]">
-      <div
-        className="w-full h-[100vh] bg-cover bg-center brightness-50 object-cover transition duration-500"
-        style={{
-          backgroundImage: `url(https://image.tmdb.org/t/p/original/${slideData[slideIndex]?.backdrop_path})`,
-        }}
-      ></div>
-      <div className="absolute top-1/3 ml-4 md:ml-16 w-8/12">
-        <h1 className="text-white text-4xl md:text-5xl h-full w-[50%] lg:text-6xl font-bold drop-shadow-lg">
-          {slideData[slideIndex]?.title}
-        </h1>
-        <p className="text-white truncate sm:whitespace-normal text-sm sm:text-base w-11/12 mt-4 sm:mt-6">
-          {slideData[slideIndex]?.overview}
-        </p>
-        <div className="flex flex-row items-center mt-4 sm:mt-6 gap-4">
-          <button className="bg-white rounded-md py-1 sm:py-2 px-2 sm:px-3 text-xs lg:text-lg font-semibold hover:bg-neutral-300 transition capitalize">
-            Play
-          </button>
-          <button className="capitalize bg-white text-white bg-opacity-30 rounded-md py-1 sm:py-2 px-2 sm:px-4 text-xs lg:text-lg font-semibold hover:bg-opacity-20 transition">
-            More info
-          </button>
-        </div>
-      </div>
-    </section>
+    <Swiper
+      grabCursor={true}
+      loop={true}
+    >
+      {slideData.map((item: SlideData, index: number) => (
+        <SwiperSlide key={index}>
+          <section className="relative h-[100vh]">
+            <div
+              className="w-full h-[100vh] bg-cover bg-center brightness-50 object-cover transition duration-500"
+              style={{
+                backgroundImage: `url(https://image.tmdb.org/t/p/original/${item.backdrop_path})`,
+              }}
+            />
+            <div className="absolute top-1/3 ml-4 md:ml-16 w-8/12 left-0">
+              <h1 className="text-white text-4xl md:text-5xl h-full w-full sm:w-[50%] lg:text-6xl font-bold drop-shadow-lg">
+                {item.title}
+              </h1>
+              <p className="text-white truncate sm:whitespace-normal text-sm sm:text-base w-11/12 mt-4 sm:mt-6">
+                {item.overview}
+              </p>
+              <div className="flex flex-row items-center mt-4 sm:mt-6 gap-4">
+                <button className="bg-white rounded-md py-1 sm:py-2 px-2 sm:px-3 text-xs lg:text-lg font-semibold hover:bg-neutral-300 transition capitalize">
+                  Play
+                </button>
+                <button className="capitalize bg-white text-white bg-opacity-30 rounded-md py-1 sm:py-2 px-2 sm:px-4 text-xs lg:text-lg font-semibold hover:bg-opacity-20 transition">
+                  More info
+                </button>
+              </div>
+            </div>
+            <div className="absolute hidden sm:flex top-1/3 right-0 mr-4 md:mr-16 w-2/12 rounded-md shadow-lg">
+              <img
+                className="w-full h-full object-contain rounded-md"
+                src={`https://image.tmdb.org/t/p/original/${item.poster_path}`}
+                alt={`${item.title}`}
+              />
+            </div>
+          </section>
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 };
 

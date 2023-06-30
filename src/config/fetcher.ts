12625c58d698d03abccc06dd30181c697400dd25
sonnned <alejo1garciasosa@gmail.com>
2023-06-30 18:28:56 -0500

    api fetcher

---
+++ src/config/fetcher.ts
@@ -0,0 +1,8 @@
+const API_ACCESS_TOKEN = import.meta.env.VITE_API_ACCESS_TOKEN;
+
+export const fetcher = (url: string) => fetch(url, {
+  headers: {
+    Authorization: `Bearer ${API_ACCESS_TOKEN}`,
+    'Content-Type': 'application/json;charset=utf-8',
+  },
+}).then((res) => res.json());

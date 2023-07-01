06a82ad6c7f9ac3586be26d47517010852cd2765
sonnned <alejo1garciasosa@gmail.com>
2023-07-01 14:26:25 -0500

    Adding home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,58 @@
+import { useState, Fragment, useEffect } from 'react';
+import { BASE_URL, endPoints } from '../config/endpoints';
+import { fetcher } from '../config/fetcher';
+import Slide from '../components/Slide';
+import List from '../components/List';
+import ListSkeleton from '../components/ListSkeleton';
+
+const arraySkeleton = new Array(9).fill(0);
+
+const Home = () => {
+  const [actionMovies, setActionMovies] = useState<any>([]);
+  const [comedyMovies, setComedyMovies] = useState<any>([]);
+
+  useEffect(() => {
+    async function fetchActionData() {
+      const request = await fetcher(
+        `${BASE_URL}${endPoints.fetchActionMovies}${1}`,
+      );
+      setActionMovies(request.results);
+    }
+
+    async function fetchComedyData() {
+      const request = await fetcher(
+        `${BASE_URL}${endPoints.fetchComedyMovies}${1}`,
+      );
+      setComedyMovies(request.results);
+    }
+
+    fetchActionData();
+    fetchComedyData();
+  }, []);
+
+  return (
+    <Fragment>
+      <Slide />
+      <div className="mx-4 sm:mx-16 pb-2 sm:pb-4">
+        {actionMovies.length ? (
+          <List
+            movies={actionMovies}
+            title="Action Movies"
+          />
+        ) : (
+          <ListSkeleton arr={arraySkeleton} />
+        )}
+        {comedyMovies.length ? (
+          <List
+            movies={comedyMovies}
+            title="Comedy Movies"
+          />
+        ) : (
+          <ListSkeleton arr={arraySkeleton} />
+        )}
+      </div>
+    </Fragment>
+  );
+};
+
+export default Home;
